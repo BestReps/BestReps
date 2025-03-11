2e3a75725464e62e0770a54cf3d9664785cf55ba
Roan <Roan.s.l.de.vries@gmail.com>
2025-03-11 15:50:19 +0100

    3 "HOT RIGHT NOW"

--- web/loadData.js
+++ web/loadData.js
@@ -10,15 +10,33 @@ document.addEventListener("DOMContentLoaded", function () {
   autocompleteList.className = "autocomplete-list";
   searchInput.parentNode.appendChild(autocompleteList);
 
-  // Static Air Force One product data
-  const staticProduct = {
-    name: "airforce one",
-    price_usd: "$13",
-    image_url:
-      "https://lh7-rt.googleusercontent.com/sheetsz/AHOq17GPbpPR9U63iaaG8yPJDjVdVSS640_cYFzS6vP7-0yTM0ZDv_3pyhEsdDVdB7VUKgRUpTIlem52UhGCjI7LptLBA_TlwiBwKwedtyGV4Ie6g2WSK4BozeDQvDQdN3_Gm9ndqXqcIw=w204-h163?key=p2g3-rzV9ITo2xi2WG5U_ZHr",
-    link: "https://cnfans.com/product/?shop_type=weidian&id=7240332207&ref=201196",
-    category: "other",
-  };
+  // Static products data
+  const staticProducts = [
+    {
+      name: "airforce one",
+      price_usd: "$13",
+      image_url:
+        "https://lh7-rt.googleusercontent.com/sheetsz/AHOq17GPbpPR9U63iaaG8yPJDjVdVSS640_cYFzS6vP7-0yTM0ZDv_3pyhEsdDVdB7VUKgRUpTIlem52UhGCjI7LptLBA_TlwiBwKwedtyGV4Ie6g2WSK4BozeDQvDQdN3_Gm9ndqXqcIw=w204-h163?key=p2g3-rzV9ITo2xi2WG5U_ZHr",
+      link: "https://cnfans.com/product/?shop_type=weidian&id=7240332207&ref=201196",
+      category: "other",
+    },
+    {
+      name: "airforce one mulitiple colour ways",
+      price_usd: "$30",
+      image_url:
+        "https://lh7-rt.googleusercontent.com/sheetsz/AHOq17G1bjriR0FrMFBTOetvjlw0pVHg00uRAYeutTB--U-sbyPkycOYDLTDUwOsYuRcwfXHA3FjJKIDM3U89z2IMgX_E8lCxgL0WfQ-bWhmbayKVU_NYiuWK7jw6g0eGy2PTmxBMAJYPw=w204-h149?key=p2g3-rzV9ITo2xi2WG5U_ZHr",
+      link: "https://cnfans.com/product/?shop_type=weidian&id=7240332207&ref=201196",
+      category: "other",
+    },
+    {
+      name: "resell batch jordan 4",
+      price_usd: "$27",
+      image_url:
+        "https://lh7-rt.googleusercontent.com/sheetsz/AHOq17FwT3QhtVTtoA5gp2oPNgjLBzBitpaj9t24bSaeamxZ-HYcm1rWUeCdHkcOFVNoNhBRKzGM8CTmYPISC9dB2VljvXJtXTgH0hGMT9Dis3rADG4I7PcrbulkkL47UrFVrAn0RYwy=w204-h149?key=p2g3-rzV9ITo2xi2WG5U_ZHr",
+      link: "https://cnfans.com/product/?shop_type=weidian&id=7264688429&ref=201196",
+      category: "shoes",
+    },
+  ];
 
   fetch("data_cat.json") // Load the categorized data
     .then((response) => {
@@ -34,11 +52,9 @@ document.addEventListener("DOMContentLoaded", function () {
       setupSearchAndAutocomplete(data); // Add this line
 
       if (isHomePage) {
-        // Only display the static product on the home page
-        console.log(
-          "On home page - displaying static Air Force One product only"
-        );
-        displayProducts([staticProduct], true);
+        // Only display the static products on the home page
+        console.log("On home page - displaying static products only");
+        displayProducts(staticProducts, true);
       } else {
         // Categorize and display products on other pages
         const currentPage = path.split("/").pop() || "index.html";
@@ -57,8 +73,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Add the search and autocomplete functionality
   function setupSearchAndAutocomplete(allProducts) {
-    // Add the static product to the search data
-    const searchProducts = [...allProducts, staticProduct];
+    // Add the static products to the search data
+    const searchProducts = [...allProducts, ...staticProducts];
 
     searchInput.addEventListener("input", function (e) {
       const searchTerm = e.target.value.toLowerCase().trim();
